a3dbf0d70f13f0b52b9a7c2298de1c1eb949dae5
vikash maurya <vikashghazipur2001@gmail.com>
2023-07-31 18:09:16 +0530

    add-airplanes

---
+++ src/seeders/20230731122815-add-airplanes.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Airplanes', [{
+         modelNumber: 'Boeing 777 ',
+         capacity:200,
+         createdAt:new Date(),
+         updatedAt:new Date()
+       },
+       {
+        modelNumber: 'Boeing 747 ',
+        capacity:300,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        modelNumber: 'Boeing 737 ',
+        capacity:250,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        modelNumber: 'Airbus A320 ',
+        capacity:200,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        modelNumber: 'Airbus A330 ',
+        createdAt:new Date(),
+        updatedAt:new Date(),
+        capacity:210
+        
+      }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
