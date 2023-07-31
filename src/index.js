3926df8239628d08ca1df84321c41c4ade06b9b3
vikash maurya <vikashghazipur2001@gmail.com>
2023-07-31 12:07:15 +0530

    added airport seeders

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const {PORT}=require('./config/serverConfig');
 const apiRoutes=require('./routes/index');
 
 const bodyParser=require('body-parser');
+const db=require('./models/index')
 
 const setupAndStartServer= async ()=>{
     // create express object
@@ -16,6 +17,10 @@ const setupAndStartServer= async ()=>{
     app.use('/api',apiRoutes);
     app.listen(PORT,async ()=>{
         console.log(`Server is starting on Port : ${PORT}`);
+        //for sync db
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true});
+        }
         
     })
 }
---
+++ src/seeders/20230730190755-add-airports.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Airports',[{
+      name:'Kempegowda International Airport',
+      cityId:8,
+      createdAt:new Date(),
+      updatedAt:new Date()
+    },
+    {
+      name:'Mysur International Airport',
+      cityId:8,
+      createdAt:new Date(),
+      updatedAt:new Date()
+    },
+    {
+      name:'Mangaluru International Airport',
+      cityId:8,
+      createdAt:new Date(),
+      updatedAt:new Date()
+    }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
